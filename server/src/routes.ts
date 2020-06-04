421beed922f1698d013d667b910c7a3ad89a287f
Victor Kayan <victor.vkal@gmail.com>
2020-06-03 23:41:52 -0300

    Create itens listing endpoint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello, word!' });
+routes.get('/itens', async (request, response) => {
+  const itens = await knex('itens').select('*');
+
+  const serverUrl = 'localhost:3333';
+  const serializedItens = itens.map(item => {
+    return {
+      title: item.title,
+      image_ur: `http://${serverUrl}/uploads/${item.image}`,
+    }
+  });
+
+  return response.json(serializedItens);
 });
 
 export default routes;
