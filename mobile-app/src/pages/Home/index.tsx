3d46b64a7c87ed31343889367275e8b6758c5a42
Victor Kayan <victor.vkal@gmail.com>
2020-06-07 23:55:22 -0300

    Create home page

---
+++ mobile-app/src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { 
+  View,
+  Text,
+  Image,
+  ImageBackground,
+} from 'react-native';
+
+import { RectButton } from 'react-native-gesture-handler';
+import { Feather as Icon } from '@expo/vector-icons';
+
+import styles from './styles';
+
+const Home: React.FC = () => {
+  return (
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      imageStyle={{ width: 274, height: 368 }}
+      style={styles.container}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontratem pontos de coleta de forma eficiente.</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <View style={styles.buttonIcon}>
+            <Icon name='arrow-right' color='#FFF' size={24} />
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  );
+}
+
+export default Home;
